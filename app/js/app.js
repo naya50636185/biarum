52d8337aeac3716190ca1458fae2145c9e6e7ca0
Anastasiya Bondarenko <naya50636185@gmail.com>
2017-01-22 14:13:52 +0400

    Changed jade, scss, added js for carousel

---
+++ app/js/app.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+    $('.slider-carousel').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
