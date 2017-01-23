3c41192e43dda172c9b4d2e7323c608f2b301e9b
Nastya Bondarenko <n.bond@ag-media.ru>
2017-01-23 13:36:49 +0300

    Changed js

--- app/js/app.js
+++ app/js/app.js
@@ -2,23 +2,22 @@ $( document ).ready(function() {
     $('.slider-carousel').slick({
         dots: false,
         arrows: true,
-        infinite: true,
         speed: 300,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 1
                 }
             },
             {
@@ -40,10 +39,10 @@ $( document ).ready(function() {
         slidesToScroll: 1
     });
 
-    $('a[href^="#"]').click(function(){
-        var el = $(this).attr('href');
-        $('body').animate({
-            scrollTop: $(el).offset().top}, 1000);
+    $('.menu-side a[href^="#"]').click(function () {
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top;
+        $('html, body').animate( { scrollTop: destination }, 1000 );
         return false;
     });
 });
